e7ca3d58d71f08f34d644ce41bbd3ffce256c6bf
ApexFatality93 <90066549+ApexFatality93@users.noreply.github.com>
2025-03-14 23:52:42 -0500

    Update getRegions.ts
    Update new EisHub regions

--- src/getRegions.ts
+++ src/getRegions.ts
@@ -14,24 +14,33 @@ export function getRegions(galaxy: string): Set<string> {
 	];
 
 	const EisHubGlyphs = [
-		'FA556C30',
-		'FA555C30',
-		'FA555C31',
-		'FA556C31',
-		'FA557C31',
-		'FA557C30',
-		'FA557C2F',
-		'FA556C2F',
-		'FA555C2F',
-		'F8556C30',
-		'F8555C30',
-		'F8555C31',
-		'F8556C31',
-		'F8557C31',
-		'F8557C30',
-		'F8557C2F',
-		'F8556C2F',
-		'F8555C2F',
+		'09556D30',
+		'09555D30',
+		'09555D31',
+		'09556D31',
+		'09557D31',
+		'09557D30',
+		'09557D2F',
+		'09556D2F',
+		'09555D2F',
+		'0A556D30',
+		'0A555D30',
+		'0A555D31',
+		'0A556D31',
+		'0A557D31',
+		'0A557D30',
+		'0A557D2F',
+		'0A556D2F',
+		'0A555D2F',
+		'08556D30',
+		'08555D30',
+		'08555D31',
+		'08556D31',
+		'08557D31',
+		'08557D30',
+		'08557D2F',
+		'08556D2F',
+		'08555D2F',
 	];
 
 	const CalHubGlyphs = [
@@ -74,8 +83,8 @@ export function getRegions(galaxy: string): Set<string> {
 	];
 	
 	if (galaxy == 'Eissentam') {
-		regionGlyphs.splice(-2, 2, ...EisHubGlyphs);
-	} 
+	    return new Set(EisHubGlyphs);
+	}
 	else if (galaxy == 'Calypso') {
 		regionGlyphs.splice(-2, 2, ...CalHubGlyphs);
 	}
